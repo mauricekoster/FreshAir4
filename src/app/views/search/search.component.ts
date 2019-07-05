b3afd5386cc755ff10253dedbb58685dcee10ca8
Maurice Koster <maurice@mauricekoster.com>
2019-07-05 16:17:07 +0200

    Search page layout complete

--- src/app/views/search/search.component.ts
+++ src/app/views/search/search.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import * as moment from 'moment';
+import {CustomValidators} from 'ngx-custom-validators';
 
 @Component({
   selector: 'app-search',
@@ -15,14 +17,32 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit() {
+    const d = moment().add(14, 'days');
+
     this.searchForm = this.formBuilder.group({
-      departureCity: [''],
-      destinationCity: ['']
+      departureCity: ['', Validators.required],
+      destinationCity: ['', Validators.required],
+      ret: ['NO', Validators.required],
+      departureDate: ['', CustomValidators.minDate(d.format('YYYY-MM-DD'))],
+      returningDate: [''],
+      adt: [1, Validators.required],
+      chd: [0],
+      inf: [0],
+      faretype: ['BESTPRICE', Validators.required]
       // password: ['', Validators.required]
     });
   }
 
+  get departureCity() {
+    return this.searchForm.get('departureCity');
+  }
+
   onSubmit() {
-    console.log('submitted');
+    const d = moment().add(14, 'days');
+    if (this.searchForm.invalid) {
+      console.log('form is invalid');
+      return;
+    }
+    console.log('submitted' + d.calendar());
   }
 }
